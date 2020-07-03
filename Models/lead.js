48cb9bf672757ab912a6c176f21e018b51ca95c2
Narasimha Kamath <narasimhakamath@outlook.com>
2020-07-03 11:10:00 +0530

    Added Lead attributes: isConverted - To check if the lead is converted or not, and createdAt - To store the date and time when the lead was created in the system.

--- Models/lead.js
+++ Models/lead.js
@@ -5,7 +5,10 @@ const leadSchema = mongoose.Schema({
 	firstName: {type: String, required: true},
 	lastName: {type: String},
 	phoneNumber: {type: String, required: true},
-	mailAddress: {type: String, required: true, unique: true}
+	mailAddress: {type: String, required: true, unique: true},
+	isConverted: {type: Boolean, default: false}
+}, {
+	timestamps: {createdAt: 'created_at'}
 });
 
 module.exports = mongoose.model("Lead", leadSchema);
